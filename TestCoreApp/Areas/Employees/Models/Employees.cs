4fea4b2084552dab86990013c66c3bfa9f4bcc2d
F <faisalalangari@gmail.com>
2025-07-27 12:08:42 -0700

    Improvement

---
+++ TestCoreApp/Areas/Employees/Models/Employees.cs
@@ -0,0 +1,12 @@
+﻿namespace TestCoreApp.Areas.Employees.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? EmployeePhone { get; set; }
+        public string? EmployeeEmail { get; set; }
+        public int? EmployeeAge { get; set; }
+        public decimal? EmployeeSalary { get; set; }
+    }
+}
--- TestCoreApp/Data/AppDbContext.cs
+++ TestCoreApp/Data/AppDbContext.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TestCoreApp.Areas.Employees.Models;
 using TestCoreApp.Models;
 
 namespace TestCoreApp.Data
 {
    public class AppDbContext : DbContext
     {           //Contructor 1- DbContextsOptions is a generic which contain our class 2- options will be sent to DbContext constructor 
+
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { //we need to define item class datatypes 
 
         }
         public DbSet<Item> Items { get; set; } //All datatypes in class item model are defined 
         
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Employees> Employees { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+           // base.OnModelCreating(modelBuilder);  
+
             modelBuilder.Entity<Category>().HasData(
                  new Category() {Id = 1, Name = "Electric Machine" },
                 new Category() { Id = 2, Name = "Computer" },
